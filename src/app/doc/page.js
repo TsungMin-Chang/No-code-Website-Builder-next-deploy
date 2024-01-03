06a7cd623e97a9bf0552862b1449f017f947086f
Sandra <sandychang37@gmail.com>
2024-01-03 17:24:38 +0800

    delete doc

--- src/app/doc/page.js
+++
@@ -1,9 +0,0 @@
-import ReactSwagger from '@/app/doc/swagger';
-
-export default async function IndexPage() {
-  return (
-    <section className="container">
-      <ReactSwagger />
-    </section>
-  );
-}
